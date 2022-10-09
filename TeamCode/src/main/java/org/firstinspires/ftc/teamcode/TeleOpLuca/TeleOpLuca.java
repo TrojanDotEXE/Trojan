0fa48026173fdf3847117b0915cb827bdefcb439
SnowyLolz <snwflake29@gmail.com>
2022-10-09 21:45:15 +0300

    am scris TeleOp

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpLuca/TeleOpLuca.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode.TeleOpLuca;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.teamcode.robot.Robot;
+
+@TeleOp(name = "TeleOpLuca", group = "")
+public class TeleOpLuca extends OpMode {
+
+    public Robot robot;
+
+    @Override
+    public void init() {
+
+        robot.initialize();
+
+    }
+
+    @Override
+    public void loop() {
+
+        double leftPower;
+        double rightPower;
+
+        double drive = -gamepad1.left_stick_y;
+        double turn  =  gamepad1.right_stick_x;
+        leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
+        rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+
+        robot.rightRear.setPower(rightPower);
+        robot.rightFront.setPower(rightPower);
+        robot.leftFront.setPower(leftPower);
+        robot.leftFront.setPower(leftPower);
+
+       if(gamepad2.a){
+
+           robot.claw.setPosition(0.5);
+       }
+       else{
+
+           robot.claw.setPosition(1);
+       }
+    }
+}
